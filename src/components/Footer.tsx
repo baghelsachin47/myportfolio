73bfcaf13c4ef9474e0664bb00c14fb9b07449b4
baghelsachin47 <baghel.sachin47@gmail.com>
2025-02-05 00:08:36 +0530

    footer correction

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-transparent text-white py-4">
       <div className="container mx-auto text-center">
-        <p className="text-sm">© 2025 John Doe. All rights reserved.</p>
+        <p className="text-sm">© 2025 Sachin Baghel.</p>
       </div>
     </footer>
   );
